4c9b506f4c0a817f154618e6b9527e351a34d9da
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2022-08-31 10:50:42 -0300

    Update produtor_consumidor.py

--- produtor_consumidor.py
+++ produtor_consumidor.py
@@ -1,45 +1,50 @@
-from random import randint
-from time import sleep
-import sys
-import os
+import threading
+import time, random
+import threading
 
-list = []
-sleep_c = False
-sleep_p = False
-list_length = int(input("Digite o tamanho da lista: "))
+class BufferLimitado:
+   TAM_BUFFER = 5
+   mutex  = threading.Semaphore(1)
+   empty  = threading.Semaphore(TAM_BUFFER)
+   full   = threading.Semaphore(0)
+   buffer = list(range(TAM_BUFFER))
+   cheio  = 0
+   livre  = 0
 
-def producer():
-    global list_length, sleep_c, sleep_p, list
+   def insert(self, item):
+      self.empty.acquire()
+      self.mutex.acquire()
+      self.buffer[self.livre] = item
+      self.livre = (self.livre + 1) % self.TAM_BUFFER
+      self.mutex.release()
+      self.full.release()
 
-    if not sleep_p:
-        while len(list) < list_length:
-            list.append(randint(0, 1000))
-            print("Produtor: Produzindo valores á lista: " + str(list))
+   def remove(self):
+      self.full.acquire()
+      self.mutex.acquire()
+      item = self.buffer[self.cheio]
+      self.cheio = (self.cheio + 1) % self.TAM_BUFFER
+      self.mutex.release()
+      self.empty.release()
+      return item
 
-    else:
-        print("Produtor: Todas as posições da lista foram preenchidas." + str(list))
-        print("Produtor saiu para descansar.Ele voltará quando a lista estiver vazia.")
-        sleep_p = True
-        sleep_c = False
-        consumer()
+b = BufferLimitado()
 
-def consumer():
-  global list_length, list, sleep_p, sleep_c
+def produtor():
+   while True:
+      time.sleep(2)
+      item = time.ctime()
+      b.insert(item)
+      print ("Produtor produziu:", b.livre, b.cheio)
+      print("--------------------------------------------------------------------")
 
-if not sleep_c:
-    if list:
-        del list[0]
-        print("Consumidor: Consumindo os valores da lista: " + str(list))
-else:
-    print("Consumidor: Todas as posições da lista foram consumidas." +str(list))
-    print("O consumidor saiu para descansar. Ele voltará quando a lista conter algum valor" )
-    sleep_c = True
-    sleep_p = False
-    producer()
+def consumidor():
+   while True:
+      time.sleep(2)
+      item = b.remove()
+      print ("Consumidor consumiu:",  b.livre, b.cheio)
+      print("--------------------------------------------------------------------")
+threading._start_new_thread(produtor, ())
+threading._start_new_thread(consumidor, ())
 
-while True:
-    producer()
-    consumer()
-    sleep(6)
-    print("--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--")
-
+while 1: pass
